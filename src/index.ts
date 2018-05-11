76ae2eaf292a0e8c232b9a4bd9875e6089ff57de
Weeco <weeco91@gmail.com>
2018-05-11 17:03:23 +0200

    Export all models and enums at the root level

--- src/index.ts
+++ src/index.ts
@@ -9,17 +9,46 @@ export * from './fortnite-client';
 // -------------------------------------------------------------------------
 // Commonly used models
 // -------------------------------------------------------------------------
-export * from './models/status/status';
+export * from './models/leaderboard/leaderboard-stats-item';
+export * from './models/leaderboard/leaderboard';
+export * from './models/lookup/lookup';
+export * from './models/news/athenamessage';
+export * from './models/news/loginmessage';
+export * from './models/news/message';
+export * from './models/news/news';
+export * from './models/news/platform-message';
+export * from './models/news/subgameselectdata';
 export * from './models/news/welcome';
 export * from './models/stats/player-stats';
+export * from './models/stats/stats-item';
+export * from './models/status/launcher-info';
+export * from './models/status/status';
+export * from './models/store/alteration';
+export * from './models/store/attributes';
+export * from './models/store/catalog-entry';
+export * from './models/store/gift-info';
+export * from './models/store/item-grant';
+export * from './models/store/meta-asset-info';
+export * from './models/store/meta-info';
+export * from './models/store/payload';
+export * from './models/store/price';
+export * from './models/store/requirement';
 export * from './models/store/store';
-export * from './models/lookup/lookup';
+export * from './models/store/storefront';
 
 // -------------------------------------------------------------------------
 // Commonly used interfaces
 // -------------------------------------------------------------------------
 export * from './interfaces/fortnite-client-options.interface';
 export * from './interfaces/fortnite-client-credentials.interface';
+
+// -------------------------------------------------------------------------
+// Commonly used enums
+// -------------------------------------------------------------------------
+export * from './enums/group-type.enum';
+export * from './enums/leaderboard-type.enum';
+export * from './enums/platform.enum';
+export * from './enums/time-window.enum';
 
 // -------------------------------------------------------------------------
 // Deprecated
