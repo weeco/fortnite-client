8373d06b8ddea695f2f8f61307afd2a3c9e56b2e
Weeco <weeco91@gmail.com>
2018-05-11 17:03:47 +0200

    Add integration test for leaderboard stats item

--- test/integration/leaderboards.spec.ts
+++ test/integration/leaderboards.spec.ts
@@ -1,11 +1,16 @@
 import { expect } from 'chai';
 import { describe, it } from 'mocha';
-import { GroupType } from '../../src/enums/group-type.enum';
-import { LeaderboardType } from '../../src/enums/leaderboard-type.enum';
-import { Platform } from '../../src/enums/platform.enum';
-import { TimeWindow } from '../../src/enums/time-window.enum';
-import { FortniteClient, IFortniteClientCredentials, Lookup } from '../../src/index';
-import { Leaderboard } from '../../src/models/leaderboard/leaderboard';
+import {
+  FortniteClient,
+  GroupType,
+  IFortniteClientCredentials,
+  Leaderboard,
+  LeaderboardStatsItem,
+  LeaderboardType,
+  Lookup,
+  Platform,
+  TimeWindow
+} from '../../src/index';
 import { api } from './init.spec';
 
 describe('Leaderboards method', () => {
@@ -24,5 +29,34 @@ describe('Leaderboards method', () => {
     );
     expect(r.statWindow).to.be.a('string');
     expect(r.entries.length).to.be.equal(100);
+  }).timeout(6 * 1000);
+
+  // tslint:disable-next-line:mocha-no-side-effect-code
+  it('should return the global wins leaderboards with (-de)serialization', async () => {
+    const r: Leaderboard = await api.getLeaderboards(
+      LeaderboardType.Wins,
+      Platform.PC,
+      GroupType.Solo,
+      TimeWindow.Weekly,
+      100
+    );
+    const json: {} = r.toJson();
+    expect(json).to.be.an('object');
+    const deserialized: Leaderboard = Leaderboard.FROM_JSON(json);
+  }).timeout(6 * 1000);
+
+  // tslint:disable-next-line:mocha-no-side-effect-code
+  it('should return a stats item with (-de)serialization', async () => {
+    const r: Leaderboard = await api.getLeaderboards(
+      LeaderboardType.Wins,
+      Platform.PC,
+      GroupType.Solo,
+      TimeWindow.Weekly,
+      100
+    );
+    const item: LeaderboardStatsItem = r.entries[0];
+    const json: {} = item.toJson();
+    expect(json).to.be.an('object');
+    const deserialized: LeaderboardStatsItem = LeaderboardStatsItem.FROM_JSON(json);
   }).timeout(6 * 1000);
 });
