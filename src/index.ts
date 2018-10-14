714be0b604120693e1f57c4e8b1a73cc6a966f12
Weeco <weeco91@gmail.com>
2018-10-15 00:05:58 +0200

    Remove unnecessary libs and interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
-// tslint:disable-next-line:no-import-side-effect
-import 'reflect-metadata';
-
 // -------------------------------------------------------------------------
 // Commonly used exports
 // -------------------------------------------------------------------------
